81dd21a8219adbe56c51ac68a4558122eecfbcd7
meanoldgit <alexantonaranda@gmail.com>
2024-06-19 00:41:05 +0200

    screen class added

---
+++ src/main/java/com/chat/Screen.java
@@ -0,0 +1,38 @@
+package com.chat;
+
+import java.io.IOException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+public class Screen {
+    private int width = 0;
+    private int height = 0;
+    private Terminal terminal;
+    
+    public Screen() {
+        try {
+            terminal = TerminalBuilder.builder().system(true).build();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean resized() {
+        return width != terminal.getWidth() || height != terminal.getHeight();
+    }
+    
+    public void resetSize() {
+        width = terminal.getWidth();
+        height = terminal.getHeight();
+        System.out.println(width + "x" + height);
+    }
+    
+    public int getWidth() {
+        return terminal.getWidth();
+    }
+
+    public int getHeight() {
+        return terminal.getHeight();
+    }
+}
