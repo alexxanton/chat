b01ea3263938ca15aa48d74b5f1be4c1fb728c11
meanoldgit <alexantonaranda@gmail.com>
2024-06-19 00:39:49 +0200

    screen class added

--- src/main/java/com/chat/Main.java
+++ src/main/java/com/chat/Main.java
@@ -1,18 +1,22 @@
 package com.chat;
 
-import java.io.IOException;
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            Terminal terminal = TerminalBuilder.builder().system(true).build();
-            int width = terminal.getWidth();
-            System.out.println(width);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        Screen screen = new Screen();
+        Thread thread = new Thread(() -> {
+            while (true) {
+                if (screen.resized()) {
+                    screen.resetSize();
+                }
+
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
