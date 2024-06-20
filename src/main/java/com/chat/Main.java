8f929914660d9efefa8b9e90e0e3bfba907d9b30
meanoldgit <alexantonaranda@gmail.com>
2024-06-21 00:35:35 +0200

    peer to peer logic added

--- src/main/java/com/chat/Main.java
+++ src/main/java/com/chat/Main.java
@@ -1,22 +1,98 @@
 package com.chat;
 
-public class Main {
-    public static void main(String[] args) {
-        Screen screen = new Screen();
-        Thread thread = new Thread(() -> {
-            while (true) {
-                if (screen.resized()) {
-                    screen.resetSize();
-                }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-                try {
-                    Thread.sleep(1000);
+public class Main {
+    private final int PORT = 3000;
+    // private String ipAddress = "192.168.1.114";
+    private String ipAddress = "172.17.24.193";
+    private boolean loop = true;
+    
+    public static void main(String[] args) {
+        Main chat = new Main();
+        chat.enterIP();
+        // chat.handleScreenResize();
+        chat.startServer();
+        chat.startClient();
+    }
+
+    public void startServer() {
+        Thread thread = new Thread(() -> {
+            try {
+                ServerSocket server = new ServerSocket(PORT);
+                while (loop) {
+                    try {
+                        Socket client = server.accept();
+                        System.out.println(client.getInetAddress());
+                        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                        out.println("hello");
+                        client.close();
+                    }
+                    catch (Exception e) {
+                        System.err.println("Error. " + e.getMessage() + ".");
+                    }
+                    threadSleep();
                 }
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                server.close();
+            }
+            catch (IOException e) {
+                System.err.println("Couldnt connect to client. " + e.getMessage() + ".");
             }
         });
         thread.start();
+    }
+
+    public void startClient() {
+        Thread thread = new Thread(() -> {
+            try {
+                while (loop) {
+                    Socket socket = new Socket(ipAddress, PORT);
+                    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String serverResponse = input.readLine();
+                    System.out.println(serverResponse);
+                    socket.close();
+                    threadSleep();
+                }
+            }
+            catch (Exception e) {
+                System.err.println("Couldnt connect to server. " + e.getMessage() + ".");
+            }
+        });
+        thread.start();
+    }
+
+    public void enterIP() {
+        System.out.print("Enter the IP: ");
+        Scanner scan = new Scanner(System.in);
+        ipAddress = scan.nextLine();
+        scan.close();
+    }
+
+    public void handleScreenResize() {
+        Screen screen = new Screen();
+        Thread thread = new Thread(() -> {
+            while (loop) {
+                if (screen.resized()) {
+                    screen.resetSize();
+                }
+                threadSleep();
+            }
+        });
+        thread.start();
+    }
+
+    private void threadSleep() {
+        try {
+            Thread.sleep(100);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
